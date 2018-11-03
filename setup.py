07a6166a5e9d120b503c1e43f4fb2c109ea007f0
Angeliz <cielangeliz@gmail.com>
2018-11-03 21:13:39 +0800

    feature: test

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 # encoding=utf-8
 import jieba
+import jieba.analyse
+import os
 
-seg_list = jieba.cut("我来到北京清华大学", cut_all=True)
-print("Full Mode: " + "/ ".join(seg_list))  # 全模式
+rootdir = './data/training/class1/'
+list = os.listdir(rootdir)
+print(list)
 
-seg_list = jieba.cut("我来到北京清华大学", cut_all=False)
-print("Default Mode: " + "/ ".join(seg_list))  # 精确模式
+for i in list:
+    f = open(rootdir+i, "r")
+    str = f.read()
 
-seg_list = jieba.cut("他来到了网易杭研大厦")  # 默认是精确模式
-print(", ".join(seg_list))
+    # seg_list = jieba.cut(str, cut_all=False)
+    # print("Default Mode: " + "/ ".join(seg_list))
 
-seg_list = jieba.cut_for_search("小明硕士毕业于中国科学院计算所，后在日本京都大学深造")  # 搜索引擎模式
-print(", ".join(seg_list))
+    keywords = jieba.analyse.extract_tags(str, topK=5, withWeight=False, allowPOS=())
+    print(i)
+    print(keywords)
+
+    f.close()
